cae432f6695ce159658c9e3045b649053bc54d9f
Lucas Ramalho Bueno <39516636+soulexklz@users.noreply.github.com>
2019-08-02 09:11:23 -0300

    Create pares_e_impares.js

---
+++ pares_e_impares.js
@@ -0,0 +1,15 @@
+var par = 0
+var impar = 0
+
+for (i = 0 ; i < 10 ; i++){
+  inteiros = prompt("Insira seus números")
+  inteiros = parseInt(inteiros)
+  if(inteiros % 2 == 0){
+    par++
+  }
+  else{
+    impar++
+  }
+}
+console.log("Números pares", par)
+console.log("Números impares", impar)
