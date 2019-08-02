3a7c922e5746f08a6328a541d355a6fda40a8b40
soulexklz <39516636+soulexklz@users.noreply.github.com>
2019-08-02 08:55:20 -0300

    Create potencia.js
    Cálculo de potência sem usar o **

---
+++ potencia.js
@@ -0,0 +1,8 @@
+var base = prompt("Insira sua base")
+var expoente = prompt("Insira seu expoente")
+base = parseInt(base)
+expoente = parseInt(expoente)
+var resultado = 0
+
+resultado = Math.pow(base, expoente)
+console.log("O resultado é", resultado)
