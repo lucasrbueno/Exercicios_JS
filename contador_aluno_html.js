df1064f4c8ce17491c212425be1d404474ddb35a
Lucas Ramalho Bueno <39516636+lucasrbueno@users.noreply.github.com>
2019-08-30 08:51:49 -0300

    Create contador_aluno_html.js

---
+++ contador_aluno_html.js
@@ -0,0 +1,61 @@
+function Aluno(numero) {
+  this.numero = numero
+  this.nota = parseInt(Math.random() * 100)
+  this.status = (this.nota >= 70) ? "Aprovado" : "Reprovado"
+}
+
+function start() {
+  // get the reference for the body
+  var body = document.getElementsByTagName("body")[0];
+
+  // creates a <table> element and a <tbody> element
+  var tbl = document.createElement("table");
+  var tblBody = document.createElement("tbody");
+
+  let contadorAprovado = 0
+  // creating all cells
+  for (var j = 1; j <= 20; j++) {
+    // creates a table row
+    var row = document.createElement("tr");
+    let obj = new Aluno(j.toString())
+
+
+    // Create a <td> element and a text node, make the text
+    // node the contents of the <td>, and put the <td> at
+    // the end of the table row
+    var cell = document.createElement("td");
+    var cellText = document.createTextNode(`Número ${j}`);
+    cell.appendChild(cellText);
+    row.appendChild(cell);
+
+    var cell = document.createElement("td");
+    var cellText = document.createTextNode(obj.nota);
+    cell.appendChild(cellText);
+    row.appendChild(cell);
+
+    var cell = document.createElement("td");
+    var cellText = document.createTextNode(obj.status);
+    cell.appendChild(cellText);
+    row.appendChild(cell);
+
+    if (obj.status == "Aprovado") {
+      contadorAprovado++
+    }
+
+    // add the row to the end of the table body
+    tblBody.appendChild(row);
+  }
+
+  // put the <tbody> in the <table>
+  tbl.appendChild(tblBody);
+  // appends <table> into <body>
+  body.appendChild(tbl);
+  // sets the border attribute of tbl to 2;
+  tbl.setAttribute("border", "2");
+
+  console.log("Número de aprovados " + contadorAprovado)
+  console.log("Número de reprovados " + (20 - contadorAprovado))
+
+  document.getElementById("aprovados").innerHTML = `Número de aprovados:   ${contadorAprovado} - ${((contadorAprovado)/20)*100}%`
+  document.getElementById("reprovados").innerHTML = `Número de reprovados: ${20 - contadorAprovado} - ${((20 - contadorAprovado)/20)*100}%`
+}
